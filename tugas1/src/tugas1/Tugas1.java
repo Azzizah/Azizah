badbc32bf7048bf668def89ddbb2a97bd208d1b0
Azzizah <43538787+Azzizah@users.noreply.github.com>
2018-09-24 19:06:45 +0700

    Add files via upload

---
+++ tugas1/src/tugas1/Tugas1.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Azizah
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+       int umur = 0 ;
+        boolean cantik = true ;
+       
+        System.out.print("input umur = ");
+        umur = keyboard.nextInt();
+        
+        if (umur>23 && cantik==true) {
+        System.out.println("Sangat Mau");
+    }
+        else if (umur > 23){ 
+        System.out.println("Pikir duluu");
+    }
+        else {
+        System.out.println("tidakkkkkkk");        
+        }
+    }
+    
+}
